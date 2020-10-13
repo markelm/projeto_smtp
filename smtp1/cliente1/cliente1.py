e35e72b2bd39b0187f58f4694baa1ec37533ba5a
mk <markelmacedo@gmail.com>
2020-10-13 03:10:52 -0400

    projeto smtp

---
+++ smtp1/cliente1/cliente1.py
@@ -0,0 +1,41 @@
+#cliente SMTP que se comunica sem nenhum protocolo de seguranca
+
+import smtplib #biblioteca para implementar o clinete
+import email.utils #provem funcoes utilitarias para formatacao/geracao
+from email.mime.text import MIMEText#classe de mensagens
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+#criando mensagem a ser enviada
+
+#msg = MIMEText("Bilbo esta com saudades. Pediu para vc trazer o anel.")
+#msg["To"] = email.utils.formataddr(("Aragorn", "aragorn@gondor.com"))
+#msg["From"] = email.utils.formataddr(("Frodo", "frodo@gondor.com"))
+#msg["Subject"] = "Quando voce vem pra ca?"
+
+
+msg = MIMEMultipart()
+body = "Bilbo esta com saudades. Pediu para vc trazer o anel."
+msg["To"] = "aragorn@rivendell.com"
+msg["From"] = "frodo@gondor.com"
+msg["Subject"] = "Quando voce vem pra ca?"
+
+msg.attach(MIMEText(body, 'plain'))
+
+filename = 'test.txt'
+attachment = open(filename, 'rb')
+part = MIMEBase('application', 'octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', "attachment; filename= "+filename)
+msg.attach(part)
+
+#Agora criamos o servidor SMTP que roda do lado do cliente para enviar
+server = smtplib.SMTP("127.0.0.1", 1025)
+server.set_debuglevel(True)#debug para ver a interacao de msgs
+
+#enviamos a msg
+server.sendmail("frodo@gondor.com", ["aragorn@rivendell.com"], msg.as_string())
+
+#encerra
+server.quit()
---
+++ smtp1/smtp1.py
@@ -0,0 +1,58 @@
+import smtplib
+import smtpd #bib do servidor
+import asyncore #multiplas conexoes
+import os
+
+#tabela com 
+domainTable = {'rivendell.com': ['127.0.0.2', 1026]}
+destination = 'home'
+receiver = ''
+
+def createFolder(direc):
+    print('chamando createFolder')
+    try:
+        if not os.path.exists(direc):
+            os.makedirs(direc)
+    except OSError:
+        print ('Error: creating directory. ' + direc)
+
+
+#simula request para o DNS e caso nao encontrado return a string 'Not Found'
+def DNSrequest(domain):
+    return domainTable.get('rivendell.com', 'Not Found')
+
+#classe herdada para sobrescrita do metido de processamento de mensagens
+class CustomsSMTPServer(smtpd.SMTPServer):
+    
+    #metodo de processamento de mensagens
+    def process_message(self, peer, mailfrom, rcpttos, data, mail_options=None, rcpt_options=None):
+        print("Mensagem enviada pelo IP {}".format(peer))
+        print("Mensagem enviada pelo e-mail {}".format(mailfrom))
+        print("Mensagem destinada ao e-mail {}".format(rcpttos))
+        print("Tamanho da mensagem: {}".format(len(data)))
+        print(f'{data}')
+        destination = rcpttos[0]
+        receiver = destination[:destination.find('@')]
+        #print(receiver)
+        destination = destination[destination.find('@') + 1:]
+        if destination == 'gondor.com':
+            destination = 'home'
+        #print(destination)
+        #print(domainTable[destination])
+
+        if destination != 'home':
+            d = DNSrequest(destination)
+            print('\nDESTINATION ACQUIRED!!!')
+            server_to = smtplib.SMTP(d[0], d[1])
+            server_to.sendmail(mailfrom, rcpttos, data)
+        else:
+            createFolder(f'./{receiver}/Inbox/')
+            f = open(f'./{receiver}/Inbox/msg_{mailfrom}.txt', 'w+')
+            f.write(f'{data}')
+
+server = CustomsSMTPServer(("127.0.0.1", 1025), None)
+
+print("Servidor em execucao")
+asyncore.loop()#entra em loop para aceitar multiplas conexoes(se necessario)
+
+
